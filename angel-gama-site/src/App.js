621e2337334b5e0c78cc7b391e0d8f7472d2687d
Angel Gama <angelgamaxd@gmail.com>
2023-08-14 17:51:48 -0600

    Added initial advice section

--- angel-gama-site/src/App.js
+++ angel-gama-site/src/App.js
@@ -7,15 +7,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my website! I'm currently working on it, so please check back later for more content.
+        </p>
+        
+        <p>
+          For now, visit my LinkedIn or Google Developer Profile :D
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/angel-gama/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href="https://developers.google.com/profile/u/Angel_Gama"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Google Developer Profile
         </a>
       </header>
     </div>

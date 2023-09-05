37b26a5b34a1ba5bfb9e6e4826d3327272cf177e
Angel Gama <angelgamaxd@gmail.com>
2023-09-05 00:07:12 -0600

    Implemented user-friendly disclaimer feature and integration of local storage for tracking disclaimer acceptance

--- angel-gama-site/src/App.js
+++ angel-gama-site/src/App.js
@@ -4,11 +4,14 @@ import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Career } from './components/Career';
 import { Footer } from './components/Footer';
+import { Disclaimer } from './components/Disclaimer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
     <div className="App">
+      <Disclaimer />
       <NavBar />
       <Banner />
       <Skills />
---
+++ angel-gama-site/src/components/Disclaimer.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export const Disclaimer = () => {
+
+    const [showDisclaimer, setShowDisclaimer] = useState(false);
+
+    useEffect(() => {
+        const disclaimerAccepted = localStorage.getItem('disclaimerAccepted');
+
+        if (disclaimerAccepted !== 'true') {
+            setShowDisclaimer(true);
+        }
+    }, []);
+
+    const handleAcceptDisclaimer = () => {
+        localStorage.setItem('disclaimerAccepted', 'true');
+        setShowDisclaimer(false);
+    };
+
+    return (
+        <div className={`disclaimer${showDisclaimer ? '' : ' accepted'}`}>
+            <p className="disclaimer-tittle">🚧 This website is under construction 🛠️</p>
+            <p>I appreciate your patience as I work on improving it. Your feedback is valuable to me, so please don't hesitate to share your thoughts on my LinkedIn.</p>
+            <button onClick={handleAcceptDisclaimer}>Accept</button>
+        </div>
+    )
+}

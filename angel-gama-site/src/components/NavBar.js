3712fd8f6f7cfb0ec6cba78c982aa28f15c7a652
Angel Gama <angelgamaxd@gmail.com>
2023-09-05 19:56:33 -0600

    Implemented collapse outside behavior for Navbar menu

--- angel-gama-site/src/components/NavBar.js
+++ angel-gama-site/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,6 +10,8 @@ export const NavBar = () => {
 
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
+    const [expanded, setExpanded] = useState(false);
+    const menuRef = useRef(null);
 
     useEffect(() => {
         const onScroll = () => {
@@ -28,20 +30,39 @@ export const NavBar = () => {
 
     const onUpdateActiveLink = (link) => {
         setActiveLink(link);
+        setExpanded(false);
     }
+
+    const handleToggle = () => {
+        setExpanded(!expanded);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (expanded && menuRef.current && !menuRef.current.contains(event.target)) {
+                setExpanded(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [expanded]);
 
     return (
 
-        <Navbar expand="lg" className={scrolled ? 'scrolled' : ''} collapseOnSelect>
+        <Navbar expand="lg" className={scrolled ? 'scrolled' : ''} expanded={expanded}>
             <Container>
                 <Navbar.Brand href="#home">
                     <img src={imagePaths.mylogo} alt="My Logo" className="my-icon" />
                     <p className="my-name">Angel Garfias</p>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" >
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle}                >
                     <span className="navbar-toggler-icon"></span>
                 </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav" ref={menuRef}>
                     <Nav className="me-auto">
                         <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>About Me</Nav.Link>
                         <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>

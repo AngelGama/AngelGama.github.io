b50f33f49bc7e6adaf9ee88c4615e9480ad0ad18
Angel Gama <angelgamaxd@gmail.com>
2023-09-19 13:20:51 -0600

    Updated the NavBar component to dynamically apply the active class to navbar links based on the scroll position

--- angel-gama-site/src/components/NavBar.js
+++ angel-gama-site/src/components/NavBar.js
@@ -15,16 +15,34 @@ export const NavBar = () => {
     const menuRef = useRef(null);
     const toggleRef = useRef(null);
 
+    const calculateActiveSection = () => {
+        const scrollY = window.scrollY;
+
+        const homeOffset = 70;
+        const skillsOffset = document.getElementById('skills').offsetTop - (2 * homeOffset);
+        const careerOffset = document.getElementById('career').offsetTop - (2 * homeOffset);
+
+        if (scrollY >= careerOffset) {
+            setActiveLink('career');
+        } else if (scrollY >= skillsOffset) {
+            setActiveLink('skills');
+        } else {
+            setActiveLink('home');
+        }
+    };
+
     useEffect(() => {
         const onScroll = () => {
+            calculateActiveSection();
             if (window.scrollY > 500) {
                 setScrolled(true);
             } else {
                 setScrolled(false);
             }
-        }
+        };
 
         window.addEventListener('scroll', onScroll);
+        calculateActiveSection();
 
         return () => window.removeEventListener('scroll', onScroll);
 
@@ -33,7 +51,7 @@ export const NavBar = () => {
     const onUpdateActiveLink = (link) => {
         setActiveLink(link);
         setExpanded(false);
-    }
+    };
 
     const handleToggle = () => {
         setExpanded(!expanded);
@@ -44,7 +62,7 @@ export const NavBar = () => {
             top: 0,
             behavior: 'smooth',
         });
-    }
+    };
 
     useEffect(() => {
         const handleClickOutside = (event) => {
@@ -73,9 +91,9 @@ export const NavBar = () => {
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav" ref={menuRef}>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>About Me</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                        <Nav.Link href="#career" className={activeLink === 'career' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('career')}>Career</Nav.Link>
+                        <Nav.Link href="#home" className="navbar-link" active={activeLink === 'home'} onClick={() => onUpdateActiveLink('home')}>About Me</Nav.Link>
+                        <Nav.Link href="#skills" className="navbar-link" active={activeLink === 'skills'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                        <Nav.Link href="#career" className="navbar-link" active={activeLink === 'career'} onClick={() => onUpdateActiveLink('career')}>Career</Nav.Link>
                     </Nav>
                     <span className='navbar-toggle'>
                         <Toggle />
